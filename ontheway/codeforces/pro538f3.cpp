52ff8a31e4c512fb613b812d34ab3d0d6e7591e4
Zhaoyi Zhang <zyz@zyz-macbookpro2.roam.corp.google.com>
2015-05-24 15:43:01 -0700

    binary index tree

---
+++ ontheway/codeforces/pro538f3.cpp
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<string.h>
+#include<algorithm>
+using namespace std;
+#define maxn 201000
+
+typedef long long int LD;
+
+// binary index tree. bit tree
+struct BitTree {
+  LD c[maxn];
+  inline int lowbit(int x) {
+    return x & (-x);
+  }
+
+  void modify(int pos, LD x) {
+    while (pos < maxn) {
+      c[pos] += x;
+      pos += lowbit(pos);
+    }
+  }
+  LD sum(int end) {
+    LD sum = 0;
+    while (end) {
+      sum += c[end];
+      end -= lowbit(end);
+    }
+    return sum;
+  }
+}tree;
+
+int i,j,n,m,k;
+
+pair<int, int> p[maxn];
+
+int ans[maxn];
+
+int main() {
+  scanf("%d",&n);
+  for(i=1;i<=n;i++) {
+    scanf("%d",&j);
+    p[i].first = j;
+    p[i].second = i;
+  }
+  sort(&p[1], &p[1+n]);
+  i=j=1;
+  for(i=1;i<=n;i++) {
+    for(k=1;k<n;k++){
+      int l = k*(p[i].second-1)+2;
+      if (l>n)
+        break;
+      int r = k*p[i].second+1;
+      if(r>n)
+        r=n;
+      ans[k] += tree.sum(r) - tree.sum(l-1);
+    }
+    tree.modify(p[i].second, 1);
+  }
+  for(i=1;i<n;i++){
+    printf("%d ", ans[i]);
+  }
+  printf("\n");
+  return 0;
+}
