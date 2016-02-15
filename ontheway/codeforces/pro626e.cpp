43a80960e2a9eb26a45d23bb4a7027f7dc752065
zzy.sysu@gmail.com <zzy.sysu@gmail.com>
2016-02-15 11:46:24 -0800

    fix 626e

--- ontheway/codeforces/pro626e.cpp
+++ ontheway/codeforces/pro626e.cpp
@@ -5,31 +5,44 @@ using namespace std;
 
 int p[maxn];
 
+long long int sum[maxn];
+
 int i,j,n,m;
 
 int ansi,ansk;
 double ans,nowsum,lastsum;
 
 inline void make() {
+  long long int total1, total2;
   for(i=2;i<n;i++) {
-    if((p[i-1]+p[n]-2*p[i])/3.0 > ans) {
-      ans=p[i-1]+p[n]-2*p[i];
-      ansi=i;
-      j=1;
-      lastsum=p[i];
-      nowsum=p[i];
-      while(i-j && n-j+1>i) {
-        nowsum+=p[i-j]+p[n-j+1];
-        if(nowsum/(j*2+1) < lastsum/((j-1)*2+1)) {
-          break;
-        }
-        j++;
+
+    int l = 1, r = i-1, mid;
+    if (n-i < r) r = n-i;
+    while ( l <= r) {
+      mid = (l+r) /2;
+      if (mid == 0)
+      break;
+      total1 = sum[n] - sum[n-mid] + sum[i] - sum[i-mid-1];
+      total2 = p[n-mid] + p[i-mid-1];
+      if (i-mid-1 > 0 && n-mid > i && total1 * 2 < total2 * (mid+mid+1)) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
       }
-      j--;
-      ans=(nowsum/(j*2+1));
-      ansk=j;
+    }
+    r=l;
+    if (r>0) {
+      if ((sum[n] - sum[n-r] + sum[i] - sum[i-r-1])*1.0/(r+r+1) - p[i]>ans) {
+        ans = (sum[n] - sum[n-r] + sum[i] - sum[i-r-1])*1.0/(r+r+1) - p[i];
+        ansi = i;
+        ansk= r;
+      }
     }
   }
+
+}
+
+void output() {
   if(ans>1e-9) {
     printf("%d\n", ansk*2+1);
     for(j=1;j<=ansk;j++)
@@ -47,6 +60,10 @@ int main() {
     scanf("%d", &p[i]);
   }
   sort(&p[1], &p[1+n]);
+  for(i=1;i<=n;i++) {
+    sum[i]=sum[i-1] + p[i];
+  }
   make();
+  output();
   return 0;
 }
