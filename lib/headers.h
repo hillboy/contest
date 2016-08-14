3025a6fcb2ad815fce76705cc7aa6b091c2f021b
zzy.sysu@gmail.com <zzy.sysu@gmail.com>
2016-08-14 13:56:17 -0700

    adding template header

---
+++ lib/headers.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <vector>
+#include <map>
+#include <string>
+#include <math.h>
+using namespace std;
+
+#define forlr(i,l,r) for(i=l;i<r;i++)
+#define forrl(i,r,l) for(i=r-1;i>=l;i--)
+#define for0r(i,r) forlr(i,0,r)
+
+#define forltr(i,l,r) for(i=l;i<=r;i++)
+#define for1tr(i,r) forltr(i,1,r)
+#define forrtl(i,r,l) for(i=r;i>=l;i--)
+
+#define forcnt(n) for(int cnt=n; cnt; cnt--)
+
+#define X(x) (x).first
+#define Y(x) (x).second
+
+//  ios_base::sync_with_stdio(false);
